a6fafdd40062bccd1110eba97ac34d37df603a38
Tianxi Ku <txku@thoughtworks.com>
2019-09-08 07:26:36 +0800

    init

---
+++ src/main/java/com/txku/springstartkit/SpringStartkitApplication.java
@@ -0,0 +1,13 @@
+package com.txku.springstartkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/txku/springstartkit/SpringStartkitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.txku.springstartkit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringStartkitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
