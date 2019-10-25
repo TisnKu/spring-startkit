74f6341ff3d80250b5f6d90da884954f9a399d0a
Tianxi Ku <txku@thoughtworks.com>
2019-10-25 23:15:04 +0800

    use log annotation instead of static injection

--- src/main/java/com/txku/springstartkit/infrastructure/ControllerAdvice.java
+++ src/main/java/com/txku/springstartkit/infrastructure/ControllerAdvice.java
@@ -1,27 +1,25 @@
 package com.txku.springstartkit.infrastructure;
 
 import com.txku.springstartkit.exception.StartkitException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ControllerAdvice {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ControllerAdvice.class);
-
     @ExceptionHandler(StartkitException.class)
     public ResponseEntity handleStartkitRuntimeException(StartkitException exception) {
-        LOGGER.error(exception.getMessage(), exception);
+        log.error(exception.getMessage(), exception);
         return ResponseEntity.status(exception.getStatus())
                 .body(exception);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity handleException(Exception exception) {
-        LOGGER.error(exception.getMessage(), exception);
+        log.error(exception.getMessage(), exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(exception);
     }
