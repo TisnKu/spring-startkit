fd85e558d49653f6e7f6eddf48c07a3e71b26a87
Tianxi Ku <txku@thoughtworks.com>
2019-10-26 21:50:15 +0800

    enable hystrix

--- src/main/java/com/txku/springstartkit/SpringStartkitApplication.java
+++ src/main/java/com/txku/springstartkit/SpringStartkitApplication.java
@@ -3,12 +3,14 @@ package com.txku.springstartkit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableDiscoveryClient
+@EnableHystrix
 public class SpringStartkitApplication {
 
 	public static void main(String[] args) {
